4acafc281b46792a00f0889440604a3f6b70b5c8
Arkxd <74634345+Arkxd@users.noreply.github.com>
2023-04-16 16:08:33 +0800

    Initial commit

--- src/Session/LoginServlet.java
+++ src/Session/LoginServlet.java
@@ -5,16 +5,30 @@ import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "LoginServlet", value = "/LoginServlet")
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.setContentType("text/html;charset=utf-8");
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        PrintWriter pw = response.getWriter();
+        //假设正确的用户名 是itcast 密码是123
+        if (("itcast").equals(username) && ("123").equals(password)) {
+            User user = new User();
+            user.setUsername(username);
+            user.setPassword(password);
+            request.getSession().setAttribute("user", user);
+            response.sendRedirect("/chapter05/IndexServlet");
+        } else {
+            pw.write("用户名或密码错误，登录失败");
+        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        doGet(request, response);
     }
 }
